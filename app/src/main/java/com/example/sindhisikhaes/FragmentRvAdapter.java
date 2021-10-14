9c4c9f81b17df237ca31e74eae35e9197c679c8b
Narayan Dhingra <narayandhingra290@gmail.com>
2021-10-14 14:07:53 +0530

    Added colors, added v23 layout for selected attributes that work only on API level 23+, Initialised view for NumbersFragment

--- app/src/main/java/com/example/sindhisikhaes/FragmentRvAdapter.java
+++ app/src/main/java/com/example/sindhisikhaes/FragmentRvAdapter.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
 public class FragmentRvAdapter extends RecyclerView.Adapter<FragmentRvAdapter.MyViewHolder> {
 
     private Context context;
+    private Fragment fragment;
     private List<Word> wordList;
     int colorId;
 
@@ -29,15 +32,16 @@ public class FragmentRvAdapter extends RecyclerView.Adapter<FragmentRvAdapter.My
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_rv, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_rv_item, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Word word = wordList.get(position);
+        int color = ContextCompat.getColor(context, colorId);
         holder.image.setImageResource(word.getImageId());
-        holder.constraintLayout.setBackgroundColor(colorId);
+        holder.constraintLayout.setBackgroundColor(color);
         holder.englishTranslation.setText(word.getEnglishTranslation());
         holder.sindhiTranslation.setText(word.getSindhiTranslation());
     }
@@ -56,6 +60,8 @@ public class FragmentRvAdapter extends RecyclerView.Adapter<FragmentRvAdapter.My
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+//            itemView.setBackgroundColor(colorId);
 
             image = (ImageView) itemView.findViewById(R.id.image);
             englishTranslation = (TextView) itemView.findViewById(R.id.tvEnglishTrans);
--- app/src/main/java/com/example/sindhisikhaes/fragments/NumbersFragment.java
+++ app/src/main/java/com/example/sindhisikhaes/fragments/NumbersFragment.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sindhisikhaes.FragmentRvAdapter;
 import com.example.sindhisikhaes.R;
+import com.example.sindhisikhaes.Word;
 import com.example.sindhisikhaes.databinding.FragmentRvBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class NumbersFragment extends Fragment {
 
     FragmentRvBinding binding;
+    private FragmentRvAdapter adapter;
+    private List<Word> wordList;
 
 
     public NumbersFragment() {
@@ -33,7 +41,14 @@ public class NumbersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentRvBinding.inflate(inflater, container, false);
+
+        prepareWordList();
+
+        adapter = new FragmentRvAdapter(getContext(), wordList, R.color.category_numbers);
+        binding.recyclerview.setAdapter(adapter);
+
         return binding.getRoot();
+
     }
 
     @Override
@@ -41,9 +56,37 @@ public class NumbersFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         binding = FragmentRvBinding.bind(view);
 
+        prepareWordList();
+        prepareAdapterAndRv();
+
+
+
 
     }
 
+    private void prepareAdapterAndRv() {
+        adapter = new FragmentRvAdapter(getContext(), wordList, R.color.category_numbers);
+        binding.recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerview.setAdapter(adapter);
+    }
+
+
+
+    private void prepareWordList() {
+
+
+        wordList = new ArrayList<Word>();
+        wordList.add(new Word("One", "Hik", R.drawable.number_one, R.raw.audio_number_one));
+        wordList.add(new Word("Two", "Bha", R.drawable.number_two, R.raw.audio_number_two));
+        wordList.add(new Word("Three", "Tre", R.drawable.number_three, R.raw.audio_number_three));
+        wordList.add(new Word("Four", "Char", R.drawable.number_four, R.raw.audio_number_four));
+        wordList.add(new Word("five", "Panj", R.drawable.number_five, R.raw.audio_number_five));
+        wordList.add(new Word("Six", "Ch", R.drawable.number_six, R.raw.audio_number_six));
+        wordList.add(new Word("Seven", "Sat", R.drawable.number_seven, R.raw.audio_number_seven));
+        wordList.add(new Word("Eight", "Ath", R.drawable.number_eight, R.raw.audio_number_eight));
+        wordList.add(new Word("Nine", "Nav", R.drawable.number_nine, R.raw.audio_number_nine));
+        wordList.add(new Word("Ten", "Dae", R.drawable.number_ten, R.raw.audio_number_ten));
+    }
 
 
 }
